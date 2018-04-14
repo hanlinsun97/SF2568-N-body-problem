5a91834153ddd496832d5409a213e4caf910a290
Eric Sun <sunhanlin15@icloud.com>
2018-04-14 23:22:10 +0200

    Add files via upload

--- MPI_version.c
+++ MPI_version.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include "list.h"
 #include <mpi.h>
-
+#include <unistd.h>
+#include <sys/time.h>
 // Structure defining a node in the tree
 struct node
 {
@@ -132,6 +133,16 @@ void allocate_particle(node *father, double* x, double* y, int myid){
 	child->y_max = y_MAX;
 	child->x_min = x_MIN;
 	child->y_min = y_MIN;
+	Particles = child->Particles;
+	for (j = 0; j < child->N_particle; j++)
+	{
+		child->x_center = child->x_center + x[Particles->index];
+		child->y_center = child->y_center + y[Particles->index];
+		Particles = Particles->next;
+	}
+	// Averaging
+	child->x_center = (child->x_center) / (child->N_particle);
+	child->y_center = (child->y_center) / (child->N_particle);
 }
 
 void tree_initialization(node *father, double *x, double *y)
@@ -342,51 +353,70 @@ void compute_force(node *father, int index, double *force_x, double *force_y, do
 
 int main(int argc, char* argv[])
 {
-
-	// index of particle
-	
-	int t, i, N = 500;
-	int myid, numprocess;
-	
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocess);
-	MPI_Status status;
-	
-	//printf("I'm %d of %d\n", myid, numprocess);
 	
 
-		// Memory initialization
-	double *x = (double *)calloc(N, sizeof(double));
-	double *y = (double *)calloc(N, sizeof(double));
-	double *x_new = (double *)calloc(N, sizeof(double));
-	double *y_new = (double *)calloc(N, sizeof(double));
-	double *v_x = (double *)calloc(N, sizeof(double));
-	double *v_y = (double *)calloc(N, sizeof(double));
-	double *force_x = (double *)calloc(N, sizeof(double));
-	double *force_y = (double *)calloc(N, sizeof(double));
-	List Root_Particles = NULL;
-	node *Root = calloc(1, sizeof(*Root));
+		// index of particle
 
-	// Electron properties
-	double charge_e = 1;
-	double m_e = 1;
+		int t, i;
+		FILE *fp1 = fopen("positions1.txt", "w");
+		FILE *fp2 = fopen("positions2.txt", "w");
+		FILE *fp3 = fopen("positions3.txt", "w");
+		FILE *fp4 = fopen("positions4.txt", "w");
+		int N;
+		int myid, numprocess;
+		//clock_t start, finish;
+		struct timeval start;
+		struct timeval end;
+		float diff;
 
-	// Time step
-	double dt = 0.01;
-	int N_t = 100;
+		double duration;
+		
 
-	// Cluster approximation parameter
-	double parameter = 1;
+		MPI_Init(&argc, &argv);
+		MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+		MPI_Comm_size(MPI_COMM_WORLD, &numprocess);
+		MPI_Status status;
+		for (N = 0; N <10000; N++)
+		{
+		if(N%50==0){
 
-	// Initial limits for the position distributon
-	double x_max = 10, y_max = 10;
-	double x_min = -10, y_min = -10;
+			//printf("I'm %d of %d\n", myid, numprocess);
 
-	// Position initialization
-	random_position_distribution(x_min, x_max, y_min, y_max, x, y, N);
-	
-/*
+			// Memory initialization
+			double *x = (double *)calloc(N, sizeof(double));
+			double *y = (double *)calloc(N, sizeof(double));
+			double *x_new = (double *)calloc(N, sizeof(double));
+			double *y_new = (double *)calloc(N, sizeof(double));
+			double *v_x = (double *)calloc(N, sizeof(double));
+			double *v_y = (double *)calloc(N, sizeof(double));
+			double *force_x = (double *)calloc(N, sizeof(double));
+			double *force_y = (double *)calloc(N, sizeof(double));
+			List Root_Particles = NULL;
+			node *Root = calloc(1, sizeof(*Root));
+
+			// Electron properties
+			double charge_e = 1;
+			double m_e = 1;
+
+			// Time step
+			double dt = 0.01;
+			int N_t = 100;
+
+			// Cluster approximation parameter
+			double parameter = 1;
+
+			// Initial limits for the position distributon
+			double x_max = 10, y_max = 10;
+			double x_min = -10, y_min = -10;
+			
+
+			// Position initialization
+
+			random_position_distribution(x_min, x_max, y_min, y_max, x, y, N);
+		//	start = clock();
+			gettimeofday(&start, NULL);
+
+			/*
 	// Writing initial positions
 	FILE *fp = fopen("/Users/pyl/Desktop/ParallelProject/positions.txt", "w");
 	// The first line contains : N, dt, N_t
@@ -396,33 +426,63 @@ int main(int argc, char* argv[])
 		fprintf(fp, "%f, %f\n", x[i], y[i]);
 	}
 */
-	// Initialization of the root
+			// Initialization of the root
+
+			for (i = 0; i < N; i++)
+			{
+				Root_Particles = append(i, Root_Particles);
+			}
+			Root->Particles = Root_Particles;
+			Root->N_particle = N;
+			Root->x_max = x_max;
+			Root->y_max = y_max;
+			Root->x_min = x_min;
+			Root->y_min = y_min;
+
+			allocate_particle(Root, x, y, myid);
+			tree_initialization(Root->children[myid], x, y);
+
+		//	finish = clock();
+		//	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+			gettimeofday(&end, NULL);
+			diff = (1000000.0*(end.tv_sec-start.tv_sec)+end.tv_usec-start.tv_usec)/1000000.0;
+			if (myid == 0)
+			{
+				if (N % 50 == 0){
+
+					printf("time is %f, N = %d\n",diff,N);
+					fprintf(fp1, "%f, %d\n", duration, N);
+				}
+				
+			}
 
 
-	for (i = 0; i < N; i++)
-		Root_Particles = append(i, Root_Particles);
-	Root->Particles = Root_Particles;
-	Root->N_particle = N;
-	Root->x_max = x_max;
-	Root->y_max = y_max;
-	Root->x_min = x_min;
-	Root->y_min = y_min;
+			if (myid == 1)
+			{
+				if (N%50==0){
+				//	printf("time is %f, N = %d\n",diff,N);
+					fprintf(fp2, "%f, %d\n", diff, N);
+				}
+				
+			}
+			if (myid == 2)
+			{
+				if (N%50==0){
+				//	printf("time is %f, N = %d\n",diff,N);
+					fprintf(fp3, "%f, %d\n", diff, N);
+				}
+				
+			}
+			if (myid == 3)
+			{
+				if (N%50==0){
+				//	printf("time is %f, N = %d\n",diff,N);
+					fprintf(fp4, "%f, %d\n", diff, N);
+				}
+				
+			}
 
-	
-
-	allocate_particle(Root, x, y, myid);
-	if (myid==0){
-	printf("%f\n", (Root->children[0])->x_max);
-	printf("%f\n", (Root->children[0])->x_min);
-	printf("%f\n", (Root->children[0])->y_max);
-	printf("%f\n", (Root->children[0])->y_min);
-
-	}
-	
-	tree_initialization(Root->children[myid], x, y);
-	
-	
-	/*
+			/*
 	for (t = 0; t < N_t; t++)
 	{
 		// Initialization of the tree
@@ -473,15 +533,22 @@ int main(int argc, char* argv[])
 
 	fclose(fp);
 */
-	//Free memory
-//	free(x);
-//	free(y);
-//	free(x_new);
-//	free(y_new);
-//	free(v_x);
-//	free(v_y);
-//	free(force_x);
-//	free(force_y);
+			//Free memory
+			free(x);
+			free(y);
+			free(x_new);
+			free(y_new);
+			free(v_x);
+			free(v_y);
+			free(force_x);
+			free(force_y);
+	}
+}
+	fclose(fp1);
+	fclose(fp2);
+	fclose(fp3);
+	fclose(fp4);
 	MPI_Finalize();
+
 	return 0;
 }
