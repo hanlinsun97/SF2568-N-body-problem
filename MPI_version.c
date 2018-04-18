65747a8a620d15de8a6e8e0a190f1a1e8ba38d15
Mathurin17 <38177998+Mathurin17@users.noreply.github.com>
2018-04-18 15:44:32 +0200

    Add files via upload

--- MPI_version.c
+++ MPI_version.c
@@ -19,8 +19,6 @@ struct node
 	List Particles;
 	int N_particle;
 
-	// Pointer to the root
-	struct node *root;
 };
 
 typedef struct node node;
@@ -42,108 +40,6 @@ void random_position_distribution(double x_min, double x_max, double y_min, doub
 	}
 }
 
-void allocate_particle(node *father, double* x, double* y, int myid){
-	int i, j;
-	double x_half, y_half, x_i, y_i, x_MAX, y_MAX, y_MIN, x_MIN;
-	int N_particle = father->N_particle;
-
-	List Particles = father->Particles;
-	// Memory allocation
-	for (i = 0; i < 4; i++)
-	{
-		father->children[i] = calloc(1, sizeof(*(father->children[i])));
-		(father->children[i])->Particles = NULL;
-	}
-	node *child;
-
-	// Coordinates of where we are going to cut
-	x_half = (father->x_max + father->x_min) * 0.5;
-	y_half = (father->y_max + father->y_min) * 0.5;
-
-	// For every particles in the father cube
-	for (i = 0; i < N_particle; i++)
-	{
-
-		x_i = x[Particles->index];
-		y_i = y[Particles->index];
-		// Where is the particle ? In which child subcube ?
-		// subcube 1
-		if ((x_i < x_half) && (y_i < y_half))
-		{
-			child = father->children[0];
-			child->N_particle = child->N_particle + 1; // 1 more particle
-			// Now we add the particle to the list of particles included in the subcube 1
-			child->Particles = append(Particles->index, child->Particles);
-		}
-		// subcube 2
-		if ((x_i >= x_half) && (y_i < y_half))
-		{
-			child = father->children[1];
-			child->N_particle = child->N_particle + 1; // 1 more particle
-			// Now we add the particle to the list of particles included in the subcube 1
-			child->Particles = append(Particles->index, child->Particles);
-		}
-		// subcube 3
-		if ((x_i < x_half) && (y_i >= y_half))
-		{
-			child = father->children[2];
-			child->N_particle = child->N_particle + 1; // 1 more particle
-			// Now we add the particle to the list of particles included in the subcube 1
-			child->Particles = append(Particles->index, child->Particles);
-		}
-		// subcube 4
-		if ((x_i >= x_half) && (y_i >= y_half))
-		{
-			child = father->children[3];
-			child->N_particle = child->N_particle + 1; // 1 more particle
-			// Now we add the particle to the list of particles included in the subcube 1
-			child->Particles = append(Particles->index, child->Particles);
-		}
-
-		Particles = Particles->next;
-	}
-	x_MAX = father->x_max;
-	y_MAX = father->y_max;
-	x_MIN = father->x_min;
-	y_MIN = father->y_min;
-	if (myid == 0)
-	{
-		x_MAX = x_half;
-		y_MAX = y_half;
-	}
-	if (myid == 1)
-	{
-		x_MIN = x_half;
-		y_MAX = y_half;
-	}
-	if (myid == 2)
-	{
-		x_MAX = x_half;
-		y_MIN = y_half;
-	}
-	if (myid == 3)
-	{
-		x_MIN = x_half;
-		y_MIN = y_half;
-	}
-	child = father->children[myid];
-	child->x_center = 0;
-	child->y_center = 0;
-	child->x_max = x_MAX;
-	child->y_max = y_MAX;
-	child->x_min = x_MIN;
-	child->y_min = y_MIN;
-	Particles = child->Particles;
-	for (j = 0; j < child->N_particle; j++)
-	{
-		child->x_center = child->x_center + x[Particles->index];
-		child->y_center = child->y_center + y[Particles->index];
-		Particles = Particles->next;
-	}
-	// Averaging
-	child->x_center = (child->x_center) / (child->N_particle);
-	child->y_center = (child->y_center) / (child->N_particle);
-}
 
 void tree_initialization(node *father, double *x, double *y)
 {
@@ -347,6 +243,120 @@ void compute_force(node *father, int index, double *force_x, double *force_y, do
 	}
 }
 
+
+// Bit flip function --> if myid = 000110 and mask = 000100 : result = 000010
+int bitflip(int myid, int mask){
+	int i;
+	int result;
+	result = myid ^ mask;
+	return result;
+}
+
+// Recursive doubling function for any number of process 2^D < P < 2^(D+1)
+// for each send, we are going to send everything inside the arrays of position and speed. x and y will be used to receive, and we will add information into x_new
+// and y_new
+void recursive_doubling(int myid, double* x, double* y, double* x_new, double* y_new, double* v_x, double* v_y, int numprocess, int N, int N_p, int R_p, int tag){
+	int d,i,flip;
+	// First task is to get D
+	int D = 0;
+	// Power of 2 : 2^D and 2^(D+1)
+	int p1,p2 = 1;
+	while(p2 < numprocess){
+		p1 = p2;
+		p2 = p2 * 2;
+		D = D + 1;
+	}
+	// To follow lecture 5 with M. Hanke notations
+	D = D-1;
+	// Recursive Doubling algorithm
+	if (myid >= p1){
+		MPI_Send(x_new, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD);
+		// Can we do this ?
+		MPI_Send(y_new, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD);
+	}
+	if (myid < numprocess - p1){
+		MPI_Recv(x, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
+		MPI_Recv(y, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		// For each particles
+		for(i=0;i<N;i++){
+			// If it is a new value (not the 0 from the allocation), then we copy it
+			if (x[i] != 0) x_new[i] = x[i];
+			if (y[i] != 0) y_new[i] = y[i];
+		}
+	}
+	if (myid < p1){
+		flip = 1;
+		for(d=0;d<D;d++){
+			// To avoid deadlocks :
+			MPI_Sendrecv (x_new, N, MPI_DOUBLE, bitflip(myid,flip), tag, x, N, MPI_DOUBLE, bitflip(myid,flip), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Sendrecv (y_new, N, MPI_DOUBLE, bitflip(myid,flip), tag, y, N, MPI_DOUBLE, bitflip(myid,flip), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			for(i=0;i<N;i++){
+				// If it is a new value (not the 0 from the allocation), then we copy it
+				if (x[i] != 0) x_new[i] = x[i];
+				if (y[i] != 0) y_new[i] = y[i];
+			}
+			flip = flip*2;
+		}
+	}
+	if (myid < numprocess - p1){
+		MPI_Send(x_new, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD);
+		// Can we do this ?
+		MPI_Send(y_new, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD);
+	}
+	if (myid >= p1){
+		MPI_Recv(&x, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
+		MPI_Recv(&y, N, MPI_DOUBLE, bitflip(myid,p1), tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
+		for(i=0;i<N;i++){
+			// If it is a new value (not the 0 from the allocation), then we copy it
+			if (x[i] != 0) x_new[i] = x[i];
+			if (y[i] != 0) y_new[i] = y[i];
+		}
+	}
+
+}
+
+
+// Update position and velocity of particle indexed by 'i'
+void update_position_velocity(double* x, double* y, double* x_new, double* y_new, double* v_x, double* v_y, double* force_x, double* force_y, double dt, int i){
+	v_x[i] = v_x[i] + force_x[i] * dt;
+	v_y[i] = v_y[i] + force_y[i] * dt;
+	x_new[i] = x[i] + v_x[i] * dt;
+	y_new[i] = y[i] + v_y[i] * dt;
+}
+
+// Update the extremal boundaries of the root + reser forces
+void update_boundaries_resetForces(double* x, double* y, double* x_new, double* y_new, double* force_x, double* force_y, int N, node* Root){
+	// We are going to need the max and min for boundaries!
+	double x_max = x[0];
+	double x_min = x[0];
+	double y_max = y[0];
+	double y_min = y[0];
+	int i;
+	for (i = 0; i < N; i++)
+	{
+		x[i] = x_new[i];
+		y[i] = y_new[i];
+		
+		// We also use this loop to reset the values of the forces
+		force_x[i] = 0;
+		force_y[i] = 0;
+
+		if (x[i] > x_max)
+			x_max = x[i];
+		if (x[i] < x_min)
+			x_min = x[i];
+		if (y[i] > y_max)
+			y_max = y[i];
+		if (y[i] < y_min)
+			y_min = y[i];
+	}
+	// Changing the boundaries
+	Root->x_max = x_max;
+	Root->y_max = y_max;
+	Root->x_min = x_min;
+	Root->y_min = y_min;
+}
+
 ///////////////////////////////
 //////////// MAIN /////////////
 ///////////////////////////////
@@ -354,200 +364,116 @@ void compute_force(node *father, int index, double *force_x, double *force_y, do
 int main(int argc, char* argv[])
 {
 	
+	// index of particle and time index
+	int t, i;
+	int N=10;
+	// Process number and number of process
+	int myid, numprocess;
 
-		// index of particle
 
-		int t, i;
-		FILE *fp1 = fopen("positions1.txt", "w");
-		FILE *fp2 = fopen("positions2.txt", "w");
-		FILE *fp3 = fopen("positions3.txt", "w");
-		FILE *fp4 = fopen("positions4.txt", "w");
-		int N;
-		int myid, numprocess;
-		//clock_t start, finish;
-		struct timeval start;
-		struct timeval end;
-		float diff;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocess);
+	MPI_Status status;
+	int tag = 100;
 
-		double duration;
-		
+	// Memory initialization
+	double *x = (double *)calloc(N, sizeof(double));
+	double *y = (double *)calloc(N, sizeof(double));
+	double *x_new = (double *)calloc(N, sizeof(double));
+	double *y_new = (double *)calloc(N, sizeof(double));
+	double *v_x = (double *)calloc(N, sizeof(double));
+	double *v_y = (double *)calloc(N, sizeof(double));
+	double *force_x = (double *)calloc(N, sizeof(double));
+	double *force_y = (double *)calloc(N, sizeof(double));
+	List Root_Particles = NULL;
+	node *Root = calloc(1, sizeof(*Root));
 
-		MPI_Init(&argc, &argv);
-		MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-		MPI_Comm_size(MPI_COMM_WORLD, &numprocess);
-		MPI_Status status;
-		for (N = 0; N <10000; N++)
-		{
-		if(N%50==0){
+	// Electron properties
+	double charge_e = 1;
+	double m_e = 1;
 
-			//printf("I'm %d of %d\n", myid, numprocess);
+	// Time step
+	double dt = 0.01;
+	int N_t = 100;
 
-			// Memory initialization
-			double *x = (double *)calloc(N, sizeof(double));
-			double *y = (double *)calloc(N, sizeof(double));
-			double *x_new = (double *)calloc(N, sizeof(double));
-			double *y_new = (double *)calloc(N, sizeof(double));
-			double *v_x = (double *)calloc(N, sizeof(double));
-			double *v_y = (double *)calloc(N, sizeof(double));
-			double *force_x = (double *)calloc(N, sizeof(double));
-			double *force_y = (double *)calloc(N, sizeof(double));
-			List Root_Particles = NULL;
-			node *Root = calloc(1, sizeof(*Root));
+	// Cluster approximation parameter
+	double parameter = 1;
 
-			// Electron properties
-			double charge_e = 1;
-			double m_e = 1;
+	// Initial limits for the position distributon
+	double x_max = 10, y_max = 10;
+	double x_min = -10, y_min = -10;
+	
 
-			// Time step
-			double dt = 0.01;
-			int N_t = 100;
+	// Position initialization
+	random_position_distribution(x_min, x_max, y_min, y_max, x, y, N);
 
-			// Cluster approximation parameter
-			double parameter = 1;
 
-			// Initial limits for the position distributon
-			double x_max = 10, y_max = 10;
-			double x_min = -10, y_min = -10;
-			
-
-			// Position initialization
-
-			random_position_distribution(x_min, x_max, y_min, y_max, x, y, N);
-		//	start = clock();
-			gettimeofday(&start, NULL);
-
-			/*
-	// Writing initial positions
-	FILE *fp = fopen("/Users/pyl/Desktop/ParallelProject/positions.txt", "w");
-	// The first line contains : N, dt, N_t
-	fprintf(fp, "%d, %f, %d\n", N, dt, N_t);
+	// Initialization of the first root
 	for (i = 0; i < N; i++)
 	{
-		fprintf(fp, "%f, %f\n", x[i], y[i]);
+		Root_Particles = append(i, Root_Particles);
 	}
-*/
-			// Initialization of the root
-
-			for (i = 0; i < N; i++)
-			{
-				Root_Particles = append(i, Root_Particles);
-			}
-			Root->Particles = Root_Particles;
-			Root->N_particle = N;
-			Root->x_max = x_max;
-			Root->y_max = y_max;
-			Root->x_min = x_min;
-			Root->y_min = y_min;
-
-			allocate_particle(Root, x, y, myid);
-			tree_initialization(Root->children[myid], x, y);
-
-		//	finish = clock();
-		//	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-			gettimeofday(&end, NULL);
-			diff = (1000000.0*(end.tv_sec-start.tv_sec)+end.tv_usec-start.tv_usec)/1000000.0;
-			if (myid == 0)
-			{
-				if (N % 50 == 0){
-
-					printf("time is %f, N = %d\n",diff,N);
-					fprintf(fp1, "%f, %d\n", duration, N);
-				}
-				
-			}
+	Root->Particles = Root_Particles;
+	Root->N_particle = N;
+	Root->x_max = x_max;
+	Root->y_max = y_max;
+	Root->x_min = x_min;
+	Root->y_min = y_min;
+	
+	// Data partionning : we have NUMPROCESS, and N data : each process is going to have N_p data to work on
+	int N_p = N/numprocess;
+	// But it may not be an exact division 
+	int R_p = N % numprocess;
 
 
-			if (myid == 1)
-			{
-				if (N%50==0){
-				//	printf("time is %f, N = %d\n",diff,N);
-					fprintf(fp2, "%f, %d\n", diff, N);
-				}
-				
-			}
-			if (myid == 2)
-			{
-				if (N%50==0){
-				//	printf("time is %f, N = %d\n",diff,N);
-					fprintf(fp3, "%f, %d\n", diff, N);
-				}
-				
-			}
-			if (myid == 3)
-			{
-				if (N%50==0){
-				//	printf("time is %f, N = %d\n",diff,N);
-					fprintf(fp4, "%f, %d\n", diff, N);
-				}
-				
-			}
 
-			/*
 	for (t = 0; t < N_t; t++)
 	{
-		// Initialization of the tree
+		// Initialization of the tree : every process are going to construct the tree
 		tree_initialization(Root, x, y);
-		for (i = 0; i < N; i++)
+		
+		
+		// Data partitioning 
+		for (i = myid*N_p; i < (myid+1)*N_p; i++)
 		{
 			// Computing the force for parameter i with the parameter "parameter"
 			compute_force(Root, i, force_x, force_y, x, y, parameter);
-
-			v_x[i] = v_x[i] + force_x[i] * dt;
-			v_y[i] = v_y[i] + force_y[i] * dt;
-			x_new[i] = x[i] + v_x[i] * dt;
-			y_new[i] = y[i] + v_y[i] * dt;
+			// Updating the position + velocity
+			update_position_velocity(x, y, x_new, y_new, v_x, v_y, force_x, force_y, dt, i);
 		}
-
-		// We are going to need the max and min for boundaries!
-		x_max = x[0];
-		x_min = x[0];
-		y_max = y[0];
-		y_min = y[0];
-		for (i = 0; i < N; i++)
-		{
-			x[i] = x_new[i];
-			y[i] = y_new[i];
-			force_x[i] = 0;
-			force_y[i] = 0;
-
-			if (x[i] > x_max)
-				x_max = x[i];
-			if (x[i] < x_min)
-				x_min = x[i];
-			if (y[i] > y_max)
-				y_max = y[i];
-			if (y[i] < y_min)
-				y_min = y[i];
+		
+		// Rest of the data to compute : only for some processes
+		if (myid<R_p){
+			i = numprocess*N_p + myid;
+			// Computing the force for parameter i with the parameter "parameter"
+			compute_force(Root, i, force_x, force_y, x, y, parameter);
+			// Updating the position + velocity
+			update_position_velocity(x, y, x_new, y_new, v_x, v_y, force_x, force_y, dt, i);
 		}
-		// Changing the boundaries
-		Root->x_max = x_max;
-		Root->y_max = y_max;
-		Root->x_min = x_min;
-		Root->y_min = y_min;
-		// Writing results
-		for (i = 0; i < N; i++)
-		{
-			fprintf(fp, "%f, %f\n", x[i], y[i]);
-		}
+		
+		
+		// Recursive doubling here
+		recursive_doubling(myid, x, y ,x_new, y_new, v_x, v_y, numprocess, N, N_p, R_p, tag);
+		
+		// Finally updating the root boundaries and reseting forces values
+		update_boundaries_resetForces(x, y, x_new, y_new, force_x, force_y, N, Root);
+		
+		// Waiting for all process to finish their step
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
-	fclose(fp);
-*/
-			//Free memory
-			free(x);
-			free(y);
-			free(x_new);
-			free(y_new);
-			free(v_x);
-			free(v_y);
-			free(force_x);
-			free(force_y);
-	}
-}
-	fclose(fp1);
-	fclose(fp2);
-	fclose(fp3);
-	fclose(fp4);
+	//Free memory
+	free(x);
+	free(y);
+	free(x_new);
+	free(y_new);
+	free(v_x);
+	free(v_y);
+	free(force_x);
+	free(force_y);
+	
+
 	MPI_Finalize();
 
 	return 0;
